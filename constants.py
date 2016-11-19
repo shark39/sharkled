35fc27d668638fe0bcb1b2e3503968b78136f734
paszin <pascalslogin@gmail.com>
2016-11-19 23:27:44 +0100

    modified constants

--- constants.py
+++ constants.py
@@ -1,5 +1,9 @@
 AREAS = {
-	"Wand"  : range(0, 300),
-	"All": ["Wand"]}
+	"desk"  : range(0, 50),
+	"window1": range(50, 100),
+	"middle": range(100, 150),
+	"window2": range(150, 200),
+	"rest": range(200, 300),
+	"All": ["desk", "window1"]}
 DEFECTLEDS = []
 LEDS_COUNT = 300
