718ebca68e5e10897cab554d50c805d31a0b95bc
paszin <pascalslogin@gmail.com>
2016-12-16 17:23:49 +0100

    capitalized areas

--- server/constants.py
+++ server/constants.py
@@ -1,9 +1,9 @@
 AREAS = {
-	"desk"  : range(0, 50),
-	"window1": range(50, 100),
-	"middle": range(100, 150),
-	"window2": range(150, 200),
-	"rest": range(200, 300),
+	"Desk"  : range(0, 50),
+	"Window1": range(50, 100),
+	"Middle": range(100, 150),
+	"Window2": range(150, 200),
+	"Rest": range(200, 300),
 	"All": range(300)}
 DEFECTLEDS = []
 LEDS_COUNT = 300
